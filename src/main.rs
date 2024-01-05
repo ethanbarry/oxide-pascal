b2e676193ad5f73b263fd67d0c76acbc483908d0
Ethan Barry <ethanbarry@howdytx.net>
2024-01-04 21:15:51 -0600

    Parses files!

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-use std::fs::File;
+use std::fs::{self, File};
 use std::path::PathBuf;
 
 use clap::{Parser, Subcommand};
+
+pub mod ops;
+pub mod parser;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -14,15 +17,14 @@ fn main() -> std::io::Result<()> {
     let cli = Cli::parse();
 
     // Path of the source code to open the file.
-    let file = File::open(cli.source)?;
+    let file_string = fs::read_to_string(cli.source).unwrap();
 
     // Now run it through cradle!
 
     //testing
-    let _ = parser::regexes("Hi there!");
+    let parsed_file = parser::regexes(&file_string);
+
+    dbg!(parsed_file);
 
     Ok(()) // Happy path!
 }
-
-pub mod ops;
-pub mod parser;
--- src/parser.rs
+++ src/parser.rs
@@ -2,11 +2,11 @@
  * Tokens are described as an enum, which is defined in
  * this file.
  */
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum TokenType {
     /* OPERATORS */
     // Assignment operator:
-    Assignment, //  :=
+    Assignment, //  := ✓
     // Mathematical operators:
     Addition,        //  +  ✓
     Subtraction,     //  -  ✓
@@ -20,43 +20,44 @@ pub enum TokenType {
     TimesEquals,     //  *=  ✓
     DivEquals,       //  /=  ✓
     // Relational operators:
-    Equal,        //  =
-    NotEqual,     //  <>
-    LessThan,     //  <
-    GreaterThan,  //  >
-    LessEqual,    //  <=
-    GreaterEqual, //  >=
-    In,           //  in
+    Equal,        //  = ✓
+    NotEqual,     //  <> ✓
+    LessThan,     //  < ✓
+    GreaterThan,  //  > ✓
+    LessEqual,    //  <= ✓
+    GreaterEqual, //  >= ✓
+    In,           //  in ✓
     // Specific set operators:
-    SymmetricDifference, //  ><
-    Include,             //  include
-    Exclude,             //  exclude
+    SymmetricDifference, //  >< ✓
+    Include,             //  include ✓
+    Exclude,             //  exclude ✓
     // Boolean and Bitwise operators:
-    Not, //  not
-    And, //  and
-    Or,  //  or
-    Xor, //  xor
-    Shl, //  shl  or  <<
-    Shr, //  shr  or  >>
+    Not, //  not ✓
+    And, //  and ✓
+    Or,  //  or ✓
+    Xor, //  xor ✓
+    Shl, //  shl  or  << ✓
+    Shr, //  shr  or  >> ✓
     // Class operators:
-    Is, //  is
-    As, //  as
+    Is, //  is ✓
+    As, //  as ✓
     /* SYMBOLS */
-    Quote,        // '
-    OpenBrace,    // {
-    CloseBrace,   // }
-    OpenParen,    // (
-    CloseParen,   // )
-    OpenBracket,  // [
-    CloseBracket, // ]
-    Dot,          // .
-    Comma,        // ,
-    Colon,        // :
-    Caret,        // ^
-    AtSign,       // @
-    DollarSign,   // $
-    HashSign,     // #
-    Ampersand,    // &
+    Quote,        // ' ✓
+    OpenBrace,    // { ✓
+    CloseBrace,   // } ✓
+    OpenParen,    // ( ✓
+    CloseParen,   // ) ✓
+    OpenBracket,  // [ ✓
+    CloseBracket, // ] ✓
+    Semicolon,    // : ✓
+    Dot,          // . ✓
+    Comma,        // , ✓
+    Colon,        // : ✓
+    Caret,        // ^ ✓
+    AtSign,       // @ ✓
+    DollarSign,   // $ ✓
+    HashSign,     // # ✓
+    Ampersand,    // & ✓
     /* BASIC KEYWORDS */
     Absolute,
     Array,
@@ -102,91 +103,462 @@ pub enum TokenType {
     Var,
     While,
     With,
+    // Identifiers
+    Number,
+    Identifier,
+    // Errors
+    Error,
 }
 
+#[derive(Debug)]
 pub struct Token {
     token_type: TokenType,
     value: Option<String>,
 }
 
 impl Token {
+    /// Returns the TokenType associated with this Token.
     pub fn get_type(&self) -> TokenType {
         self.token_type
     }
 
+    /// Returns the optional string value of this Token.
+    /// This value is only present in identifiers, numbers,
+    /// and errors.
     pub fn get_value(&self) -> Option<String> {
-        self.value
+        self.value.to_owned()
+    }
+
+    /// Returns true if this Token is of type TokenType::Error.
+    pub fn is_error(&self) -> bool {
+        if self.token_type == TokenType::Error {
+            true
+        } else {
+            false
+        }
     }
 }
 
-/// Accepts a character, and the one succeeding it,
-/// and returns a tuple of (Token, i8) where the
-/// Token contains the value of the symbol, and the
-/// i8 represents the distance to advance `c` by.
-///
-/// For example, |'+', '='| returns a tuple of
-/// (Token::PlusEquals, 2) because the loop must
-/// consume the next char twice.
-// pub fn char_parse_operator(c: char, next: char) -> (Token, i8) {
-//     match c {
-//         '+' => match next {
-//             // If a `=` follows it was PlusEquals.
-//             '=' => (Token::PlusEquals, 2),
-//             _ => (Token::Addition, 1),
-//         },
-//         '-' => match next {
-//             // Same idea.
-//             '=' => (Token::MinusEquals, 2),
-//             _ => (Token::Subtraction, 1),
-//         },
-//         '*' => match next {
-//             '=' => (Token::TimesEquals, 2),
-//             '*' => (Token::Exponentiation, 2),
-//             _ => (Token::Multiplication, 1),
-//         },
-//         '/' => match next {
-//             '=' => (Token::DivEquals, 2),
-//             _ => (Token::Division, 1),
-//         },
-//         _ => todo!(),
-//     }
-// }
-
-// Later return custom error types. To be used in an iterator map() fn...
-pub fn parse(s: &str) -> Result<Token, std::io::Error> {
+/// Accepts a single token-type string from regexes() and parses it into a Token.
+fn parse(s: &str) -> Token {
     // First construct the match statements for all possiblities:
-    match s {
+    match s.to_lowercase().as_str() {
         ":=" => Token {
             token_type: TokenType::Assignment,
             value: None,
         },
-        _ => todo!(), // Return errors?
-    };
-
-    todo!()
+        "<=" => Token {
+            token_type: TokenType::LessEqual,
+            value: None,
+        },
+        ">=" => Token {
+            token_type: TokenType::GreaterEqual,
+            value: None,
+        },
+        "+=" => Token {
+            token_type: TokenType::PlusEquals,
+            value: None,
+        },
+        "-=" => Token {
+            token_type: TokenType::MinusEquals,
+            value: None,
+        },
+        "*=" => Token {
+            token_type: TokenType::TimesEquals,
+            value: None,
+        },
+        "/=" => Token {
+            token_type: TokenType::DivEquals,
+            value: None,
+        },
+        "<>" => Token {
+            token_type: TokenType::NotEqual,
+            value: None,
+        },
+        "><" => Token {
+            token_type: TokenType::SymmetricDifference,
+            value: None,
+        },
+        "**" => Token {
+            token_type: TokenType::Exponentiation,
+            value: None,
+        },
+        "<<" | "shl" => Token {
+            token_type: TokenType::Shl,
+            value: None,
+        },
+        ">>" | "shr" => Token {
+            token_type: TokenType::Shr,
+            value: None,
+        },
+        "=" => Token {
+            // Moving on to single-character operators & symbols...
+            token_type: TokenType::Equal,
+            value: None,
+        },
+        "+" => Token {
+            token_type: TokenType::Addition,
+            value: None,
+        },
+        "-" => Token {
+            token_type: TokenType::Subtraction,
+            value: None,
+        },
+        "*" => Token {
+            token_type: TokenType::Multiplication,
+            value: None,
+        },
+        "/" => Token {
+            token_type: TokenType::Division,
+            value: None,
+        },
+        "%" | "mod" => Token {
+            token_type: TokenType::Modulus,
+            value: None,
+        },
+        ">" => Token {
+            token_type: TokenType::GreaterThan,
+            value: None,
+        },
+        "<" => Token {
+            token_type: TokenType::LessThan,
+            value: None,
+        },
+        "'" => Token {
+            token_type: TokenType::Quote,
+            value: None,
+        },
+        "[" | "(." => Token {
+            token_type: TokenType::OpenBracket,
+            value: None,
+        },
+        "]" | ".)" => Token {
+            token_type: TokenType::CloseBracket,
+            value: None,
+        },
+        "." => Token {
+            token_type: TokenType::Dot,
+            value: None,
+        },
+        "," => Token {
+            token_type: TokenType::Comma,
+            value: None,
+        },
+        "(" => Token {
+            token_type: TokenType::OpenParen,
+            value: None,
+        },
+        ")" => Token {
+            token_type: TokenType::CloseParen,
+            value: None,
+        },
+        ":" => Token {
+            token_type: TokenType::Colon,
+            value: None,
+        },
+        ";" => Token {
+            token_type: TokenType::Semicolon,
+            value: None,
+        },
+        "^" => Token {
+            token_type: TokenType::Caret,
+            value: None,
+        },
+        "@" => Token {
+            token_type: TokenType::AtSign,
+            value: None,
+        },
+        "{" | "(*" => Token {
+            // These tokens both indicate comments.
+            token_type: TokenType::OpenBrace,
+            value: None,
+        },
+        "}" | "*)" => Token {
+            // Ditto.
+            token_type: TokenType::CloseBrace,
+            value: None,
+        },
+        "$" => Token {
+            token_type: TokenType::DollarSign,
+            value: None,
+        },
+        "#" => Token {
+            token_type: TokenType::HashSign,
+            value: None,
+        },
+        "&" => Token {
+            token_type: TokenType::Ampersand,
+            value: None,
+        },
+        // Beginning keywords!
+        "in" => Token {
+            token_type: TokenType::In,
+            value: None,
+        },
+        "not" => Token {
+            token_type: TokenType::Not,
+            value: None,
+        },
+        "and" => Token {
+            token_type: TokenType::And,
+            value: None,
+        },
+        "or" => Token {
+            token_type: TokenType::Or,
+            value: None,
+        },
+        "xor" => Token {
+            token_type: TokenType::Xor,
+            value: None,
+        },
+        "include" => Token {
+            token_type: TokenType::Include,
+            value: None,
+        },
+        "exclude" => Token {
+            token_type: TokenType::Exclude,
+            value: None,
+        },
+        "is" => Token {
+            token_type: TokenType::Is,
+            value: None,
+        },
+        "as" => Token {
+            token_type: TokenType::As,
+            value: None,
+        },
+        "absolute" => Token {
+            token_type: TokenType::Absolute,
+            value: None,
+        },
+        "array" => Token {
+            token_type: TokenType::Array,
+            value: None,
+        },
+        "asm" => Token {
+            token_type: TokenType::Asm,
+            value: None,
+        },
+        "begin" => Token {
+            token_type: TokenType::Begin,
+            value: None,
+        },
+        "case" => Token {
+            token_type: TokenType::Case,
+            value: None,
+        },
+        "const" => Token {
+            token_type: TokenType::Const,
+            value: None,
+        },
+        "constructor" => Token {
+            token_type: TokenType::Constructor,
+            value: None,
+        },
+        "destructor" => Token {
+            token_type: TokenType::Destructor,
+            value: None,
+        },
+        "do" => Token {
+            token_type: TokenType::Do,
+            value: None,
+        },
+        "downto" => Token {
+            token_type: TokenType::Downto,
+            value: None,
+        },
+        "else" => Token {
+            token_type: TokenType::Else,
+            value: None,
+        },
+        "end" => Token {
+            token_type: TokenType::End,
+            value: None,
+        },
+        "file" => Token {
+            token_type: TokenType::File,
+            value: None,
+        },
+        "for" => Token {
+            token_type: TokenType::For,
+            value: None,
+        },
+        "function" => Token {
+            token_type: TokenType::Function,
+            value: None,
+        },
+        "goto" => Token {
+            token_type: TokenType::Goto,
+            value: None,
+        },
+        "if" => Token {
+            token_type: TokenType::If,
+            value: None,
+        },
+        "implementation" => Token {
+            token_type: TokenType::Implementation,
+            value: None,
+        },
+        "inherited" => Token {
+            token_type: TokenType::Inherited,
+            value: None,
+        },
+        "inline" => Token {
+            token_type: TokenType::Inline,
+            value: None,
+        },
+        "interface" => Token {
+            token_type: TokenType::Interface,
+            value: None,
+        },
+        "label" => Token {
+            token_type: TokenType::Label,
+            value: None,
+        },
+        "nil" => Token {
+            token_type: TokenType::Nil,
+            value: None,
+        },
+        "object" => Token {
+            token_type: TokenType::Object,
+            value: None,
+        },
+        "of" => Token {
+            token_type: TokenType::Of,
+            value: None,
+        },
+        "operator" => Token {
+            token_type: TokenType::Operator,
+            value: None,
+        },
+        "packed" => Token {
+            token_type: TokenType::Packed,
+            value: None,
+        },
+        "procedure" => Token {
+            token_type: TokenType::Procedure,
+            value: None,
+        },
+        "program" => Token {
+            token_type: TokenType::Program,
+            value: None,
+        },
+        "record" => Token {
+            token_type: TokenType::Record,
+            value: None,
+        },
+        "reintroduce" => Token {
+            token_type: TokenType::Reintroduce,
+            value: None,
+        },
+        "repeat" => Token {
+            token_type: TokenType::Repeat,
+            value: None,
+        },
+        "self" => Token {
+            token_type: TokenType::SelfKeyword,
+            value: None,
+        },
+        "set" => Token {
+            token_type: TokenType::Set,
+            value: None,
+        },
+        "string" => Token {
+            token_type: TokenType::StringKeyword,
+            value: None,
+        },
+        "then" => Token {
+            token_type: TokenType::Then,
+            value: None,
+        },
+        "to" => Token {
+            token_type: TokenType::To,
+            value: None,
+        },
+        "type" => Token {
+            token_type: TokenType::Type,
+            value: None,
+        },
+        "unit" => Token {
+            token_type: TokenType::Unit,
+            value: None,
+        },
+        "until" => Token {
+            token_type: TokenType::Until,
+            value: None,
+        },
+        "uses" => Token {
+            token_type: TokenType::Uses,
+            value: None,
+        },
+        "var" => Token {
+            token_type: TokenType::Var,
+            value: None,
+        },
+        "while" => Token {
+            token_type: TokenType::While,
+            value: None,
+        },
+        "with" => Token {
+            token_type: TokenType::With,
+            value: None,
+        },
+        // Catch numeric literals.
+        s if s.chars().all(|c| char::is_digit(c, 10)) => Token {
+            token_type: TokenType::Number,
+            value: Some(s.to_owned()),
+        },
+        // Catch any leftovers, which must be identifiers...
+        s if s.chars().all(char::is_alphanumeric) => Token {
+            token_type: TokenType::Identifier,
+            value: Some(s.to_owned()),
+        },
+        "//" => Token {
+            token_type: TokenType::Error,
+            value: Some(format!("`{}` is not yet a supported comment format.", s)),
+        },
+        _ => {
+            eprintln!("[ WARN ]: Unknown character(s) `{}`", s);
+            Token {
+                token_type: TokenType::Error,
+                value: Some(s.to_owned()),
+            }
+        }
+    } // End match; return this expression.
 }
 
-// Applies the regexes to a string, which is the whole file at once, and returns
-// a Vec of the split substrings.
-pub fn regexes(s: &str) -> Vec<String> {
+/// Applies the regexes to a string, which is the whole file at once, and returns
+/// a Vec of the matched tokens.
+pub fn regexes(s: &str) -> Vec<Token> {
     use regex::Regex;
 
     // Captures the syntax of a normal language okay... Tweak for Pascal!
-    let lexer_regex = Regex::new(r#"(?x)(?::=|\+=|\-=|\*=|/=|<=|>=|==|!=|&&|\|\||<<|>>)|(?:\+|-|\*|/|=|<|>|!|&|\||\^|%|~)|(?:\(|\)|\{|\}|\[|\]|\.|,|;|:)|\d+|[a-zA-Z]+"#).unwrap();
+    // EDIT: Mostly tweaked; it captures every character on the list, I believe.
+    //       Check for anything extraneous that could throw us off...
+    let lexer_regex = Regex::new(r#"(?x)(?::=|\+=|\-=|\*=|/=|<=|>=|==|<>|&&|//|\(\*|\*\)|\(\.|\.\)|<<|>>)|(?:\+|-|\*|/|=|<|>|!|&|\||\^|%|~)|(?:\(|\)|\{|\}|\[|\]|\.|,|;|:)|\d+|[a-zA-Z]+"#).unwrap();
 
-    let code_string = "foo: x = 5 + 3 ** (y - 2);";
-    for cap in lexer_regex.captures_iter(code_string) {
-        println!("{}", cap.get(0).unwrap().as_str());
-    }
+    // Testing data; may come in handy:
+    // let code_string = "foo: x = 5 + 3 * (y - 2);";
+    //                   " ' + - * / = < > [ ] . , ( ) : ^ @ { } $ # & % << >> ** <> >< <= >= := += -= *= /= (* *) (. .) // ";
 
-    let result: Vec<String> = lexer_regex
-        .captures_iter(code_string)
+    // Get a vec of tokens from the regex on the input.
+    let syntax_parse: Vec<Token> = lexer_regex
+        .captures_iter(s)
         .map(|cap| cap.get(0).unwrap().as_str().to_string())
+        .into_iter()
+        .map(|s| parse(&s))
+        .into_iter()
+        .inspect(|token| {
+            if token.is_error() {
+                match token.get_value() {
+                    Some(err_msg) => eprintln!("{}", err_msg),
+                    None => eprintln!("[ WARN ]: Error Token with no error message value! This state should be impossible!")
+                }
+            }
+        })
         .collect();
 
-    dbg!(result);
-
-    todo!()
+    syntax_parse
 }
 
 // TODO: Write tests for the regexes/parser functions!
